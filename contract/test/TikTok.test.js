b2cce2703d7f9f086d3922ff54663fed5a12b011
colorssk <ming.yan@sumscope.com>
2022-10-13 18:17:26 +0800

    test getBalance

--- contract/test/TikTok.test.js
+++ contract/test/TikTok.test.js
@@ -4,29 +4,24 @@ const { accounts, contract } = require("@openzeppelin/test-environment");
 const Tiktok = contract.fromArtifact("TikTok");
 // Start test block
 describe('TikTok', function () {
-  const [ deployer, provider ] = accounts;
+  const [ deployer, provider1,  provider2] = accounts;
   const INIT_CONTRACT_BALANCE = ether('50');
+  const CHECK_TOTAL_BALANCE = ether('100');
   before(async function(){
     this.tiktok = await Tiktok.new({ from: deployer});
-    await this.tiktok.deposite({ from: provider, value: INIT_CONTRACT_BALANCE });
+    await this.tiktok.deposite({ from: provider1, value: INIT_CONTRACT_BALANCE });
+    await this.tiktok.deposite({ from: provider2, value: INIT_CONTRACT_BALANCE });
   })
 
-  it('test before', async function(){
+  it('init contract check balance of contract', async function(){
     expect(await balance.current(this.tiktok.address)).to.be.bignumber.eq(
-      INIT_CONTRACT_BALANCE
+      CHECK_TOTAL_BALANCE
     );
   })
 
-  // it('test part', async function () {
-    
-  //   expect(await balance.current(this.tiktok.address)).to.be.bignumber.eq(
-  //     INIT_CONTRACT_BALANCE
-  //   );
-  // });
-
-
-  // it('deposite', async function () {
-    
-  //   expect(await this.box.retrieve()).to.be.bignumber.equal(value);
-  // });
+  it('get balance refer to msg.sender', async function(){
+    expect(await this.tiktok.getBalance(provider1)).to.be.bignumber.eq(
+      INIT_CONTRACT_BALANCE
+    );
+  })
 });
